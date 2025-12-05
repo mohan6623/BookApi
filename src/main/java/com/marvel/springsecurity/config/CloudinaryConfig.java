d4776fb0b37ed99502ce8f6e51b087eb5af25b66
mohan6623 <mohanapasath6623@gmail.com>
2025-12-05 09:11:59 +0530

    make this project production ready!

---
+++ src/main/java/com/marvel/springsecurity/config/CloudinaryConfig.java
@@ -0,0 +1,37 @@
+package com.marvel.springsecurity.config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String cloudApiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String cloudApiSecret;
+
+//    @Bean
+//    public Cloudinary cloudinary(){
+//        Map<String, String> config = new HashMap<>();
+//        config.put("cloud_name", cloudName);
+//        config.put("api_key", cloudApiKey);
+//        config.put("api_secret", cloudApiSecret);
+//        return new Cloudinary(config);
+//    }
+    @Bean
+    public Cloudinary cloudinary(){
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", cloudApiKey,
+                "api_secret", cloudApiSecret
+        ));
+    }
+}
